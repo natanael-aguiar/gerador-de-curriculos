9fe823d9b442350ce29b312daabfe67d512419c9
Natanael Oliveira <natan.kamui@gmail.com>
2025-05-29 07:57:16 -0300

    Remove comentário sobre exibição do currículo com botão de imprimir

--- gerar.php
+++ gerar.php
@@ -9,6 +9,5 @@ ob_start();
 include 'templates/curriculo_template.php';
 $curriculo = ob_get_clean();
 
-// Exibe o currículo já com o botão de imprimir centralizado
 $curriculo = str_replace('</body>', '<div style="text-align:center; margin: 32px 0 0 0;"><button onclick="window.print()" class="btn btn-success" style="font-size:1.1rem; padding: 10px 32px; border-radius:2rem; margin-top:24px;">Imprimir Currículo</button></div></body>', $curriculo);
 echo $curriculo;
